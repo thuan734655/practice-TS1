c560f8993a71afe6196fb7ce9db6a7dc6f9371d4
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2025-02-13 04:44:21 +0700

    upgrade view/component handling 'any'

--- src/views/components/Pagination.ts
+++ src/views/components/Pagination.ts
@@ -1,47 +1,70 @@
 import { RenderPaginationData } from "@/types/componentTypes";
 
 export default class Pagination {
-    public static render(state:RenderPaginationData ): void {
-        const paginationElement = document.querySelector('.pagination');
-        const totalPages = Math.ceil(state.totalItems / state.itemsPerPage);
-        
-        const createPageButton = (pageNum: number): string => `
-            <button class="pagination-btn ${pageNum === this.getPage(state) ? 'active' : ''}" data-page="${pageNum}">
+  public static render(state: RenderPaginationData): void {
+    const paginationElement = document.querySelector(".pagination");
+    const totalPages = Math.ceil(state.totalItems / state.itemsPerPage);
+
+    const createPageButton = (pageNum: number): string => `
+            <button class="pagination-btn ${
+              pageNum === state.currentPage ? "active" : ""
+            }" data-page="${pageNum}">
                 ${pageNum}
             </button>
         `;
-  
+
+    if (totalPages <= 1) {
+      if (paginationElement) {
+        paginationElement.innerHTML = "";
+      }
+    } else {
+      if (paginationElement) {
         const currentPage = this.getPage(state);
-
-        const startPage = Math.max(1, currentPage - 2);
-        const endPage = Math.min(totalPages, startPage + 4);
-
-        if (totalPages <= 1) {
-            if (paginationElement) {
-                paginationElement.innerHTML = ""; 
-            }
+        console.log(currentPage, "current page", totalPages);
+        
+        let html = "";
+        let startPage, endPage;
+        
+        // if we have 5 pages => 1,2, 5 pages are missing because they are near the end or beginning
+        if (totalPages <= 4) {
+          startPage = 1;
+          endPage = totalPages;
+        } else if (currentPage <= 2) {
+          startPage = 1;
+          endPage = 4;
+        } else if (currentPage >= totalPages - 1) {
+          startPage = totalPages - 3;
+          endPage = totalPages;
         } else {
-            if (paginationElement) {
-                const paginationContent = `${Array.from({ length: endPage - startPage + 1 }, (_, i) => createPageButton(startPage + i)).join('')}`;
-                paginationElement.innerHTML = paginationContent; 
-            }
+          startPage = currentPage - 1;
+          endPage = currentPage + 2;
         }
+        
+        for (let pageNum = startPage; pageNum <= endPage; pageNum++) {
+          html += createPageButton(pageNum);
+        }
+        
+        if (paginationElement) {
+          paginationElement.innerHTML = html;
+        }
+        
+      }
+    }
+  }
+
+  private static getPage(state: RenderPaginationData): number {
+    const { currentFilter, currentPage, pageMovies, pageTvShow } = state;
+
+    if (!currentFilter) {
+      return currentPage;
     }
 
-    private static getPage(state:RenderPaginationData ): number {
-        const { currentFilter, currentPage, pageMovies, pageTvShow } = state;
-
-        if (!currentFilter) {
-            return currentPage;
-        }
-
-        if (currentFilter === 'all') {
-            return currentPage;
-        } else if (currentFilter === 'movies') {
-            return pageMovies ?? currentPage;  
-        } else {
-            return pageTvShow ?? currentPage;
-        }
-
+    if (currentFilter === "TV Show" && pageTvShow) {
+      return pageTvShow 
+    } else if (currentFilter === "Movies" && pageMovies) {
+      return pageMovies 
+    } else {
+      return currentPage;
     }
+  }
 }
