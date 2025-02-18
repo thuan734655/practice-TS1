db7ae6b35eb06b8a59a1780536fab69f38f80bbc
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2025-02-18 23:04:37 +0700

    add field movie

--- src/constants/formFieldConfig.ts
+++ src/constants/formFieldConfig.ts
@@ -1,19 +1,31 @@
-import { FieldConfig } from "@/types/componentTypes";
+import { FieldConfig } from '@/types/componentTypes';
 
-export const fieldConfigs: Record<string, FieldConfig> = {
-  movie_name: { label: "Movie Name", type: "text", placeholder: "Enter movie name", maxlength: 255, required: true },
-  description: { label: "Description", type: "textarea", placeholder: "Enter video description", required: true },
-  rating: { label: "Rating", type: "number", placeholder: "Rate from 0 to 10", max: 10, min: "0", step: 0.1, required: true },
-  avatar: { label: "Avatar", type: "file", accept: "image/*"  },
-  background: { label: "Background", type: "file", accept: "image/*" },
-  release_date: { label: "Release Date", type: "date", min: "1900-01-01", max: "2100-12-31", required: true },
-  episode_run_time: { label: "Episode Run Time", type: "text", placeholder: "Enter run time (e.g., 1h 30m)", maxlength: 50, required: true },
-  genres: { label: "Genres", type: "text", placeholder: "Enter genres (comma separated)", maxlength: 500, required: true },
-  status: { label: "Status", type: "text", placeholder: "Enter video status", maxlength: 500, required: true },
-  last_air_date: { label: "Last Air Date", type: "date", min: "1900-01-01", max: "2100-12-31", required: false },
-  first_air_date: { label: "First Air Date", type: "date", min: "1900-01-01", max: "2100-12-31", required: false },
-  number_of_episodes: { label: "Number of Episodes", type: "number", placeholder: "Enter number of episodes", min: "1", max: 1000, required: false },
-  number_of_seasons: { label: "Number of Seasons", type: "number", placeholder: "Enter number of seasons", min: "1", max: 100, required: false },
-  type: { label: "Type", type: "select", options: ["Movie", "TV Show"], required: true },
+export const fieldConfigsUpdateAndTVShow: Record<string, FieldConfig> = {
+  type: { label: 'Type', type: 'select', options: [ 'TV Show','Movie'], required: true },
+  movie_name: { label: 'Movie Name', type: 'text', placeholder: 'Enter movie name', maxlength: 255, required: true },
+  title: { label: 'Title', type: 'text', placeholder: 'Enter title', maxlength: 255, required: true },
+  description: { label: 'Description', type: 'textarea', placeholder: 'Enter video description', required: true },
+  rating: { label: 'Rating', type: 'number', placeholder: 'Rate from 0 to 10', max: 10, min: '0', step: 0.1, required: true },
+  avatar: { label: 'Avatar', type: 'file', accept: 'image/*' },
+  background: { label: 'Background', type: 'file', accept: 'image/*' },
+  episode_run_time: { label: 'Episode Run Time', type: 'number', placeholder: 'Enter run time (e.g., 1h 30m)', maxlength: 50, required: true },
+  genres: { label: 'Genres', type: 'text', placeholder: 'Enter genres (comma separated)', maxlength: 500, required: true },
+  status: { label: 'Status', type: 'text', placeholder: 'Enter video status', maxlength: 500, required: true },
+  last_air_date: { label: 'Last Air Date', type: 'date', min: '1900-01-01', max: '2100-12-31', required: false },
+  first_air_date: { label: 'First Air Date', type: 'date', min: '1900-01-01', max: '2100-12-31', required: false },
+  number_of_episodes: { label: 'Number of Episodes', type: 'number', placeholder: 'Enter number of episodes', min: '1', max: 1000, required: false },
+  number_of_seasons: { label: 'Number of Seasons', type: 'number', placeholder: 'Enter number of seasons', min: '1', max: 100, required: false },
 };
 
+export const fieldConfigMovies: Record<string, FieldConfig> = {
+  type: { label: 'Type', type: 'select', options: ['Movie', 'TV Show'], required: true },
+  movie_name: { label: 'Movie Name', type: 'text', placeholder: 'Enter movie name', maxlength: 255, required: true },
+  title: { label: 'Title', type: 'text', placeholder: 'Enter title', maxlength: 255, required: true },
+  description: { label: 'Description', type: 'textarea', placeholder: 'Enter video description', required: true },
+  rating: { label: 'Rating', type: 'number', placeholder: 'Rate from 0 to 10', max: 10, min: '0', step: 0.1, required: true },
+  avatar: { label: 'Avatar', type: 'file', accept: 'image/*' },
+  background: { label: 'Background', type: 'file', accept: 'image/*' },
+  release_date: { label: 'Release Date', type: 'date', min: '1900-01-01', max: '2100-12-31', required: true },
+  episode_run_time: { label: 'Episode Run Time', type: 'number', placeholder: 'Enter run time (e.g., 1h 30m)', maxlength: 50, required: true },
+  genres: { label: 'Genres', type: 'text', placeholder: 'Enter genres (comma separated)', maxlength: 500, required: true },
+};
