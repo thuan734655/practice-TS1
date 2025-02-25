7dff6440570c55a9e5eb794e2a375465a43ae78c
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2025-02-25 18:45:48 +0700

    clean pagination

--- src/views/components/Pagination.ts
+++ src/views/components/Pagination.ts
@@ -2,7 +2,7 @@ import { RenderPaginationData } from '@/types/componentTypes';
 
 export default class Pagination {
   public static render(state: RenderPaginationData): void {
-    const paginationElement = document.querySelector('.pagination');
+    const paginationElement = document.querySelector('.pagination') as HTMLElement;
     const totalPages = Math.ceil(state.totalItems / state.itemsPerPage);
 
     const createPageButton = (pageNum: number): string => `
@@ -12,44 +12,39 @@ export default class Pagination {
         `;
 
     if (totalPages <= 1) {
-      if (paginationElement) {
-        paginationElement.innerHTML = '';
-      }
+      paginationElement.innerHTML = '';
     } else {
+      const currentPage = this.getPage(state);
+
+      let html = '';
+      let startPage, endPage;
+
+      // if we have 5 pages => 1,2, 5 pages are missing because they are near the end or beginning
+      switch (true) {
+        case totalPages <= 4:
+          startPage = 1;
+          endPage = totalPages;
+          break;
+        case currentPage === 1:
+          startPage = 1;
+          endPage = 4;
+          break;
+        case currentPage >= totalPages - 1:
+          startPage = totalPages - 3;
+          endPage = totalPages;
+          break;
+        default:
+          startPage = currentPage - 1;
+          endPage = currentPage + 2;
+          break;
+      }
+
+      for (let pageNum = startPage; pageNum <= endPage; pageNum++) {
+        html += createPageButton(pageNum);
+      }
+
       if (paginationElement) {
-        const currentPage = this.getPage(state);
-        console.log(currentPage, 'current page', totalPages);
-
-        let html = '';
-        let startPage, endPage;
-
-        // if we have 5 pages => 1,2, 5 pages are missing because they are near the end or beginning
-        switch (true) {
-          case totalPages <= 4:
-            startPage = 1;
-            endPage = totalPages;
-            break;
-          case currentPage === 1:
-            startPage = 1;
-            endPage = 4;
-            break;
-          case currentPage >= totalPages - 1:
-            startPage = totalPages - 3;
-            endPage = totalPages;
-            break;
-          default:
-            startPage = currentPage - 1;
-            endPage = currentPage + 2;
-            break;
-        }
-
-        for (let pageNum = startPage; pageNum <= endPage; pageNum++) {
-          html += createPageButton(pageNum);
-        }
-
-        if (paginationElement) {
-          paginationElement.innerHTML = html;
-        }
+        paginationElement.innerHTML = html;
       }
     }
   }
@@ -71,12 +66,9 @@ export default class Pagination {
   }
 
   public static isVisiblePagination(state: boolean): void {
-    const paginationElement = document.querySelector('.pagination');
-    if (paginationElement && state) {
-      (paginationElement as HTMLElement).style.visibility = 'visible';
-    }
-    if (paginationElement && !state) {
-      (paginationElement as HTMLElement).style.visibility = 'hidden';
+    const paginationElement = document.querySelector('.pagination') as HTMLElement;
+    if (paginationElement) {
+      paginationElement.style.visibility = state ? 'visible' : 'hidden';
     }
   }
 }
